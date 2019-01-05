6c8b529b774bdf1b55bad4dbb32936e467a3eca7
kshitij <kshitijagrawal2607@gmail.com>
2019-01-05 18:17:35 +0530

    New system, new commit

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path,re_path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),	
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/',views.post_draft_list,name='post_draft_list'),
+    path('post/<pk>/publish/',views.post_publish,name="post_publish"),	
+    path('post/<pk>/remove/',views.post_remove,name="post_remove"),
+    path('CP/',views.cp,name='cp'),
+    path('ML/',views.ml,name='ml'),
+    path('JEE/',views.jee,name='jee'),
+    path('AllPosts/',views.all,name='all'),	
+    re_path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    re_path(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    re_path(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+    re_path(r'^post/(?P<pk>\d+)/upvote/$', views.upvote, name='upvote'),
+    re_path(r'^post/(?P<pk>\d+)/downvote/$', views.downvote, name='downvote'),
+    re_path(r'^post/(?P<pk>\d+)/down/$', views.down, name='down'),
+    re_path(r'^post/(?P<pk>\d+)/up$', views.up, name='up'),		
+]
