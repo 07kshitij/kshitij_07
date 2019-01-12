0f4e4c3b40620478174cef21aa8a154bf1a75726
Kshitij Agrawal <35527647+07kshitij@users.noreply.github.com>
2019-01-12 17:29:08 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,8 +24,5 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),    
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
