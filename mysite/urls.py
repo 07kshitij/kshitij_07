21210330b10e43a073b35b10080829f1209e2a0f
kshitij <kshitijagrawal2607@gmail.com>
2019-06-24 21:30:50 +0530

    Modified the frontend

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,10 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),    
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
